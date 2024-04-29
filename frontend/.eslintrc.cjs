ce864c6ab1de3662cc1115bc78ec7ac032dcff2b
evermake <vladeryabkin@gmail.com>
2024-04-29 12:53:55 +0300

    feat: initial frontend implementation (WIP)

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,22 @@
+/* eslint-env node */
+module.exports = {
+  root: true,
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended'
+  ],
+  ignorePatterns: ['orval.config.cjs'],
+  overrides: [
+    {
+      files: [
+        'e2e/**/*.{test,spec}.{js,ts,jsx,tsx}'
+      ],
+      'extends': [
+        'plugin:playwright/recommended'
+      ]
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  }
+}
---
+++ frontend/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterView } from 'vue-router'
+import TheHeader from './components/TheHeader.vue';
+</script>
+
+<template>
+  <div class="container mx-auto">
+    <TheHeader />
+    <main class="py-6">
+      <RouterView />
+    </main>
+  </div>
+</template>
